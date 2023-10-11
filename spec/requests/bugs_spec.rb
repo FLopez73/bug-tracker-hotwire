0eecbc03cbedf8a310a57b5756937b2a6bafcbbd
flopez73 <flopez730@gmail.com>
2023-10-10 21:14:39 -0700

    test

--- spec/requests/bugs_spec.rb
+++ spec/requests/bugs_spec.rb
@@ -37,5 +37,31 @@ RSpec.describe "Bugs", type: :request do
       expect(bug.assignee).to eq 'Borja'
     end
   end
+
+  describe "PATCH/update" do
+    it "updates existing bug" do 
+      bug_params = {
+        bug: {
+          description: 'slow response time',
+          priority: 'Low',
+          assignee: 'Borja' 
+        }
+      }
+
+      post '/bugs', params: bug_params
+      bug = Bug.first 
+      update_params = {
+        bug: {
+          description: 'site crashing',
+          priority: 'High',
+          assignee: 'Steve' 
+        }
+      }
+      patch "/bugs/#{big.id}", params: bug_params
+      expected(response).to have_http_status(200)
+      new_bug = Bug.first
+      expect(new_bug.assignee).to eq "Steve"
     
-end
+    end
+  end
+    
