572f12b1f81e6b00fcba0b6fadf6ccc51f60cb53
flopez73 <flopez730@gmail.com>
2023-10-12 11:04:50 -0700

    need to display error still

--- spec/models/bug_spec.rb
+++ spec/models/bug_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Bug, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a description" do
+  bug = Bug.create priority:'High', assignee: 'Tom'
+  expect(bug.errors[:description]).to_not be_empty
+ end
+  it "should have a priority" do
+  bug = Bug.create description:'broken link', assignee: 'Tom'
+  expect(bug.errors[:priority]).to_not be_empty
+ end
+  it "should have a priority" do
+  bug = Bug.create description:'broken link', priority:'high'
+  expect(bug.errors[:assignee]).to_not be_empty
+ end
 end
+
